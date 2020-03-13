9387c233b9711a9d144f34ae87d69f4e279fe185
Ivan Morales <kete1987@gmail.com>
2020-03-13 19:25:49 +0100

    First commit Android App (template)

---
+++ android/app/src/main/java/com/covid19spain/MainActivity.java
@@ -0,0 +1,20 @@
+package com.covid19spain;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.provider.Settings;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.helloWorld);
+        textView.setText(Settings.Secure.getString(getContentResolver(), Settings.Secure.ANDROID_ID));
+    }
+}
